268350ea439fa728375dd9273622d7230b768375
Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>
2022-04-17 15:43:26 +0200

    FAST

---
+++ fix.sh
@@ -0,0 +1,1 @@
+dpkg --add-architecture i386
--- webi.sh
+++ webi.sh
@@ -1,1 +1,23 @@
-sudo apt-get install -y terminator git
+# Install essentials
+sudo apt-get install -y terminator git curl
+
+# Install Taskfile
+sudo snap install task --classic
+
+# Install VS Code
+sudo snap install code --classic
+
+# Created dir required by tasks
+mkdir -p .taskfiles/statusfiles
+mkdir ~/.statusfiles
+mkdir ~/Shared
+
+# Clone repository for K8s clusters management
+git clone https://github.com/krzwiatrzyk/local-cluster.git
+
+# Clone repository for Ubuntu machine management
+git clone https://github.com/krzwiatrzyk/ubuntu-setup.git
+
+# Execute Ubuntu machine basic setup
+cd ubuntu-setup
+task
