ec10205352e9ed6b95599a8434370c43c0f72885
Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>
2022-05-18 08:23:00 +0200

    feat(fast): 2022-05-18T06:23:00Z

--- webi.sh
+++ webi.sh
@@ -7,17 +7,11 @@ sudo snap install task --classic
 # Install VS Code
 sudo snap install code --classic
 
-# Created dir required by tasks
-mkdir -p .taskfiles/statusfiles
-mkdir ~/.statusfiles
 mkdir ~/Shared
 
-# Clone repository for K8s clusters management
-git clone https://github.com/krzwiatrzyk/local-cluster.git
-
 # Clone repository for Ubuntu machine management
-git clone https://github.com/krzwiatrzyk/ubuntu-setup.git
+git clone https://github.com/krzwiatrzyk/lib-system-manager.git
 
 # Execute Ubuntu machine basic setup
-cd ubuntu-setup
+cd lib-system-manager
 task
