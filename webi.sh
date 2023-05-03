4948f669c1a639f27173802871591e0768308d77
Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>
2023-05-03 13:14:17 +0200

    fast

--- webi.sh
+++ webi.sh
@@ -12,11 +12,11 @@ sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/lo
 # sudo snap install code --classic
 
 # Clone repository for machine management if it doesn't exist
-[[ ! -d "${HOME}/.task" ]] && git clone https://github.com/krzwiatrzyk/lib-system-manager.git ${HOME}/.task
+test ! -d "${HOME}/.task" && git clone https://github.com/krzwiatrzyk/lib-system-manager.git ${HOME}/.task
 cp ${HOME}/.task/modules/global/Taskfile.yaml ${HOME}/Taskfile.yaml
 
 # Execute Ubuntu machine basic setup
-cd ${HOME}/lib-system-manager
+cd ${HOME}/.task
 
 # # First task will result with error thanks to OhMyZSH installation script - it enters new shell by default
 # task || task
