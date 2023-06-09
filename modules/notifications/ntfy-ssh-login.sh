19ecdf289d522e0228328f86ca61c1ecd958423a
Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>
2023-06-09 16:30:13 +0200

    commit

--- modules/notifications/ntfy-ssh-login.sh
+++ modules/notifications/ntfy-ssh-login.sh
@@ -4,5 +4,5 @@ if [ "${PAM_TYPE}" = "open_session" ]; then
     -H prio:high \
     -H tags:warning \
     -d "SSH login: ${PAM_USER} from ${PAM_RHOST}" \
-    ntfy.windkube.com/ssh
+    https://ntfy.windkube.com/ssh
 fi
